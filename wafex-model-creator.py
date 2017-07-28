83c2eb1cafaaf0fcf8d6d40d2eac51c2622cdee7
Federico <federico@demeo.eu>
2017-07-28 14:14:34 +0200

    Change structure for creating ASLanpp model and start implementing the interface

--- wafex-model-creator.py
+++ wafex-model-creator.py
@@ -9,39 +9,19 @@ from os.path import basename
 
 from burp import IBurpExtender
 from burp import IContextMenuFactory
+from burp import ITab
 
-from javax.swing import JPanel;
-from javax.swing import JMenuItem;
+from javax.swing import JPanel
+from javax.swing import JButton
+from javax.swing import JMenuItem
+from javax.swing import JFileChooser
+from javax.swing import JTextArea
+from javax.swing.filechooser import FileNameExtensionFilter
 
 from http_parser.pyparser import HttpParser
 from urlparse import urlparse
 
-class BurpExtender(IBurpExtender, IContextMenuFactory):
-    
-    aslanpp_constants = set()
-    aslanpp_nonpublic_constants = set()
-    aslanpp_variables = set()
-    taglist = set()
-    aslanpp_tables_nonpublic_constants = set()
-    
-    # requests
-    page = "none"
-    params ="none"
-    cookie = "none"
-    cookie2 = "none"
-
-    # response
-    return_page = "none"
-    return_cookie = "none"
-
-    # concretization
-    url = "none"
-    method = "none"
-    params_concrete = []
-    cookie_concrete = []
-    headers = []
-    mapping = []
-    concretization_file = {}
+class BurpExtender(IBurpExtender, IContextMenuFactory, ITab):
 
     taglist = []
     tag = "tag"
@@ -50,10 +30,6 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
     var_tag = "Var_"
     var_tag_i = 0
     
-    branches = ""
-    params_keys = []
-    cookie_request = ""
-
     request_skeleton = """
     \t\t\t\t\t\ton(?Entity*->*Actor:http_request({},{},{}).{}.?WebNonce):{{
     \t\t\t\t\t\t% todo: request's behavior here
@@ -69,52 +45,80 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
     """
     populate_database_skeleton = "db->add({});\n"
 
-    _webapp_branch = ""
-    _client_branch = ""
-    _init_database = ""
-
     def registerExtenderCallbacks(self, callbacks):
         
         # your extension code here
         self._panel = JPanel()
+        button = JPanel()
+        run = JButton("Test", actionPerformed=self.test)
+        button.add(run)
+        self.area = JTextArea()
+
+        self._panel.add(button)
+        self._panel.add(self.area)
         self._callbacks = callbacks
         callbacks.setExtensionName("WAFEx")
-        #callbacks.addSuiteTab(self)
+        callbacks.addSuiteTab(self)
         callbacks.registerContextMenuFactory(self)
         
         return
 
-    # def getTabCaption(self):
-    #     return "Hello tab"
+    def test(self, e):
+        try:
+            print("1")
+            chooseFile = JFileChooser()
+            print("2")
+            filter_ = FileNameExtensionFilter("txt files", ["txt"])
+            print("3")
+            chooseFile.addChoosableFileFilter(filter_)
+            print("4")
 
-    # def getUiComponent(self):
-    #     return self._panel
+            ret = chooseFile.showDialog(self._panel, "Choose file")
+            print("5")
+
+            if ret == JFileChooser.APPROVE_OPTION:
+                file = chooseFile.getSelectedFile()
+        except Exception as e:
+            print(e)
+
+        print("test")
+
+    def getTabCaption(self):
+        return "WAFEx"
+
+    def getUiComponent(self):
+        return self._panel
     
     def createMenuItems(self, invocation):
         ret = []
-        #if(invocation.getInvocationContext() == invocation.CONTEXT_TARGET_SITE_MAP_TREE or invocation.getInvocationContext() == invocation.CONTEXT_TARGET_SITE_MAP_TABLE):
-        if(invocation.getInvocationContext() == invocation.CONTEXT_TARGET_SITE_MAP_TABLE):
-            menu = JMenuItem("Generate WAFEx model")
-            messages = invocation.getSelectedMessages()
-            def listener(e):
-                """ Generates a new WAFEx model. """
-                self.generateWAFExModel(messages)
-
-            menu.addActionListener(listener)
-            ret.append(menu)
+        try:
+            #if(invocation.getInvocationContext() == invocation.CONTEXT_TARGET_SITE_MAP_TREE or invocation.getInvocationContext() == invocation.CONTEXT_TARGET_SITE_MAP_TABLE):
+            if(invocation.getInvocationContext() == invocation.CONTEXT_TARGET_SITE_MAP_TABLE):
+                menu = JMenuItem("Generate WAFEx model")
+                messages = invocation.getSelectedMessages()
+                def listener(e):
+                    """ Generates a new WAFEx model. """
+                    #self.generateWAFExModel(messages)
+                    self.area.setText("PIPPO")
+                menu.addActionListener(listener)
+                ret.append(menu)
+        except Exception as e:
+            print(e)
         return ret
 
 
     def generateWAFExModel(self,messages):
         # reset some variables
-        self._webapp_branch = ""
-        self._client_branch = ""
-        self.i_tag = 0
-        self.aslanpp_constants = set()
-        self.aslanpp_nonpublic_constants = set()
-        self.aslanpp_variables = set()
-        self.taglist = set()
-        self.aslanpp_tables_nonpublic_constants = set()
+        # self._webapp_branch = ""
+        # self._client_branch = ""
+        # self.i_tag = 0
+        # self.aslanpp_constants = set()
+        # self.aslanpp_nonpublic_constants = set()
+        # self.aslanpp_variables = set()
+        # self.taglist = set()
+        # self.aslanpp_tables_nonpublic_constants = set()
+
+        model = self.AslanppModel()
 
         print("create model")
         # start the generation
@@ -126,41 +130,41 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
                 http_request = "".join(chr(b) for b in msg.getRequest() if b >= 0 and b <= 256)
                 http_response = "".join(chr(b) for b in msg.getResponse() if b >=0 and b <= 256)
                 protocol = msg.getHttpService().getProtocol()
-                self.parseHttpRequestResponse(http_request, http_response, protocol)
+                self.parseHttpRequestResponse(model, http_request, http_response, protocol)
 
             # create the database
-            self.parse_database("/Users/federicodemeo/Downloads/chained.sql")
+            self.parse_database(model, "/Users/federicodemeo/Downloads/chained.sql")
 
 
             # create a new model
             skeleton = open("skeleton.aslan++","r").read()
-            if self.aslanpp_nonpublic_constants:
-                nonpublic_constants = "nonpublic " + ",".join(item for item in self.aslanpp_nonpublic_constants) + " : text;"
+            if model._aslanpp_nonpublic_constants:
+                nonpublic_constants = "nonpublic " + ",".join(item for item in model._aslanpp_nonpublic_constants) + " : text;"
                 skeleton = skeleton.replace("@constants2",nonpublic_constants)
             else:
                 skeleton = skeleton.replace("@constants2","")
-            if self.aslanpp_constants:
-                public_constants = ",".join(item for item in self.aslanpp_constants) + " : text;"
+            if model._aslanpp_constants:
+                public_constants = ",".join(item for item in model._aslanpp_constants) + " : text;"
                 skeleton = skeleton.replace("@constants",public_constants)
             else:
                 skeleton = skeleton.replace("@constants","")
-            if self.aslanpp_variables:
-                variables = ", ".join(item for item in self.aslanpp_variables) + " : message;"
+            if model._aslanpp_variables:
+                variables = ", ".join(item for item in model._aslanpp_variables) + " : message;"
                 skeleton = skeleton.replace("@webappsymbols",variables)
             else:
                 skeleton = skeleton.replace("@webappsymbols","")
-            if self.taglist:
-                tags = ", ".join(item for item in self.taglist) + " : text;"
+            if model._taglist:
+                tags = ", ".join(item for item in model._taglist) + " : text;"
                 skeleton = skeleton.replace("@tags",tags)
             else:
                 skeleton = skeleton.replace("@tags","")
-            skeleton = skeleton.replace("@webappbody",self._webapp_branch)
-            skeleton = skeleton.replace("@honestbody",self._client_branch)
+            skeleton = skeleton.replace("@webappbody",model._webapp_branch)
+            skeleton = skeleton.replace("@honestbody",model._client_branch)
 
-            if self.aslanpp_tables_nonpublic_constants:
-                nonpublic_constants = "nonpublic " + ",".join(item for item in self.aslanpp_tables_nonpublic_constants) + " : text;"
+            if model._aslanpp_tables:
+                nonpublic_constants = "nonpublic " + ",".join(item for item in model._aslanpp_tables) + " : text;"
                 skeleton = skeleton.replace("@databasestructure",nonpublic_constants)
-                skeleton = skeleton.replace("@databaseinit",self._init_database)
+                skeleton = skeleton.replace("@databaseinit",model._init_database)
             else:
                 skeleton = skeleton.replace("@databasestructure","")
                 skeleton = skeleton.replace("@databaseinit","")
@@ -171,14 +175,14 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
             print("model created")
 
             with open("concrete.txt","w") as f:
-                f.write(json.dumps(self.concretization_file))
+                f.write(json.dumps(model._concretization_file))
            
         except Exception as e:
             print(e)
 
 
 
-    def parseHttpRequestResponse(self, http_request, http_response, protocol):
+    def parseHttpRequestResponse(self, model, http_request, http_response, protocol):
         """ Parses a HTTP Request/Response and generate it's translation in ASLan++. """
         # To keep the concretization file simple, it will contain
         # - URL
@@ -191,64 +195,67 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         request_parser.execute(http_request,len(http_request))
 
         # URL for concretization
-        self.url = protocol +"://"+ request_parser.get_headers()['Host'] +"/" + request_parser.get_url()
+        url = protocol +"://"+ request_parser.get_headers()['Host'] +"/" + request_parser.get_url()
 
         # path (this string should not begin with something different from a character)
-        self.page = re.sub("^[^a-z]*","",urlparse(self.url).path.replace(".","_").replace("/","_"))
+        page = re.sub("^[^a-z]*","",urlparse(url).path.replace(".","_").replace("/","_"))
 
         # method for concretization
-        self.method = request_parser.get_method()
+        method = request_parser.get_method()
         
         query_string = ""
         # GET requests
-        if self.method == "GET":
+        if method == "GET":
             # GET parameters
             query_string = request_parser.get_query_string()
         # POST requests
-        elif self.method == "POST":
+        elif method == "POST":
             # POST parameters
             query_string = request_parser.recv_body()
 
         # parse parameters
+        params = "none"
+        params_concrete = []
         if query_string:
             # saving the concrete parameters
-            self.params_concrete = [couple.split("=") for couple in query_string.split("&")]
+            params_concrete = [couple.split("=") for couple in query_string.split("&")]
 
             # parse the parameters and retrieve ASLan++ code, constants, variables and mapping
-            aslanpp_params, constants, variables, mapping = self.parse_parameters(self.params_concrete)
+            aslanpp_params, constants, variables, mapping = self.parse_parameters(params_concrete)
 
-            self.aslanpp_nonpublic_constants |= constants 
-            self.aslanpp_variables |= variables
-            self.mapping += mapping
+            model._aslanpp_nonpublic_constants |= constants 
+            model._aslanpp_variables |= variables
+            model._mapping += mapping
 
             # save ASLan++ parameters
-            self.params = "none" if not aslanpp_params else aslanpp_params[:-3]
+            params = "none" if not aslanpp_params else aslanpp_params[:-3]
 
         # cookie in the request
         try:
             cookie_request = request_parser.get_headers()['Cookie']
 
             simple_cookie = Cookie.SimpleCookie(cookie_request) 
-            self.cookie_concrete = [[item,simple_cookie[item].value] for item in simple_cookie]
+            cookie_concrete = [[item,simple_cookie[item].value] for item in simple_cookie]
             
             # parse the parameters and retrieve ASLan++ code, constants, variables and mapping
-            aslanpp_cookie, constants, variables, mapping = self.parse_parameters(self.cookie_concrete)
+            aslanpp_cookie, constants, variables, mapping = self.parse_parameters(cookie_concrete)
 
-            self.aslanpp_nonpublic_constants |= constants 
-            self.aslanpp_variables |= variables
-            self.mapping += mapping
+            model._aslanpp_nonpublic_constants |= constants 
+            model._aslanpp_variables |= variables
+            model._mapping += mapping
 
-            self.cookie = "none" if not aslanpp_cookie else aslanpp_cookie[:-3]
+            cookie = "none" if not aslanpp_cookie else aslanpp_cookie[:-3]
         except KeyError:
+            cookie = "none"
             pass
 
         # check if the page should be nonpublic
         page_nonpublic = self.is_nonpublic(request_parser,query_string)
 
         if page_nonpublic:
-            self.aslanpp_nonpublic_constants.add(self.page)
+            model._aslanpp_nonpublic_constants.add(page)
         else:
-             self.aslanpp_constants.add(self.page)
+             model._aslanpp_constants.add(page)
 
         # check the response
         response_parser = HttpParser()
@@ -259,14 +266,14 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         # the header. If Location is set, it means is a 302 Redirect
         # and the client is receiving a different page back in the response
         try:
-            self.return_page = urlparse(response_parser.get_headers()['Location']).path.partition("?")[0].replace("/","_")
+            return_page = urlparse(response_parser.get_headers()['Location']).path.partition("?")[0].replace("/","_")
             is_nonpublic = self.is_nonpublic(response_parser,"")
             if is_nonpublic:
-                self.aslanpp_nonpublic_constants.add(self.return_page)
+                model._aslanpp_nonpublic_constants.add(return_page)
             else:
-                self.aslanpp_constants.add(self.return_page)
+                model._aslanpp_constants.add(return_page)
         except KeyError:
-            self.return_page = self.page
+            return_page = page
 
         # and let's see if we have a new cookie
         try:
@@ -278,23 +285,24 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
             # parse the parameters and retrieve ASLan++ code, constants, variables and mapping
             aslanpp_cookie, constants, variables, mapping = self.parse_parameters(cookies)
 
-            self.aslanpp_nonpublic_constants |= constants 
-            self.aslanpp_variables |= variables
-            self.mapping += mapping
+            model._aslanpp_nonpublic_constants |= constants 
+            model._aslanpp_variables |= variables
+            model._mapping += mapping
 
-            self.return_cookie = "none" if not aslanpp_cookie else aslanpp_cookie[:-3]
+            return_cookie = "none" if not aslanpp_cookie else aslanpp_cookie[:-3]
         except KeyError:
+            return_cookie = "none"
             pass
 
         tag = self.tag + str(self.i_tag)
-        self._webapp_branch += self.request_skeleton.format(self.page, self.params,self.cookie,tag,self.return_page,self.return_cookie,tag)
-        self._client_branch += self.client_skeleton.format(self.page, self.params,self.cookie,tag,self.return_page,self.return_cookie,tag)
+        model._webapp_branch += self.request_skeleton.format(page, params,cookie,tag,return_page,return_cookie,tag)
+        model._client_branch += self.client_skeleton.format(page, params,cookie,tag,return_page,return_cookie,tag)
 
         # create the concretization JSON
-        self.concretization_file[tag] = {"method" : self.method, "url" : self.url, "params" : self.params_concrete,"cookie":self.return_cookie}
+        model._concretization_file[tag] = {"method" : method, "url" : url, "params" : params_concrete,"cookie":return_cookie}
         
         # save tag in taglist and increment the tag number
-        self.taglist.add("{}{}".format(self.tag,self.i_tag))
+        model._taglist.add("{}{}".format(self.tag,self.i_tag))
         self.i_tag +=1
 
 
@@ -324,16 +332,14 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
 
         return aslanpp_code, constants, variables, mapping
 
-    def parse_database(self, sql_file):
-
+    def parse_database(self, model, sql_file):
+        """ Parses a SQL file, extracts the tables and populate the model class. """
         with open(sql_file) as f:
             line = f.read()
             tables_name = re.findall("CREATE TABLE[a-zA-Z ]*`(.*)`", line)
             for t in tables_name:
-                self.aslanpp_tables_nonpublic_constants.add(t)
-                self._init_database += self.populate_database_skeleton.format(t)
-
-
+                model._aslanpp_tables.add(t)
+                model._init_database += self.populate_database_skeleton.format(t)
 
 
     def is_nonpublic(self,header,body):
@@ -358,3 +364,16 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         else:
             return True
 
+    class AslanppModel:
+        """ Represents a new ASLan++ model. Create an object for each model
+        and pass it the parsing method. """
+        _aslanpp_constants = set()
+        _aslanpp_nonpublic_constants = set()
+        _aslanpp_variables = set()
+        _mapping = []
+        _taglist = set()
+        _aslanpp_tables = set()
+        _concretization_file = {}
+        _webapp_branch = ""
+        _client_branch = ""
+        _init_database = ""
